74a336541793f5caef9de8f711ccc9a5332faa34
Iker Martínez Velasco <138805316+moimenta84@users.noreply.github.com>
2025-04-27 23:05:54 +0200

    Update UsersRepository.java

--- main/java/com/juego7th/repository/UsersRepository.java
+++ main/java/com/juego7th/repository/UsersRepository.java
@@ -1,8 +1,7 @@
 package com.juego7th.repository;
-
 import com.juego7th.modelo.Users;
-
 public class UsersRepository extends GenericRepository<Users,Integer>{
+    
     public UsersRepository() {
         super(Users.class);
     }
